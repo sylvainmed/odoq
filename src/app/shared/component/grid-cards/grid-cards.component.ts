db53858cfc7318bd6a2bd57c3d3420f0dcabadce
Sylvain <medard.sylvain@gmail.com>
2020-08-19 18:40:31 +0200

    correction du calcul de la hauteur à l'affiche de la grid de quotes

--- src/app/shared/component/grid-cards/grid-cards.component.ts
+++ src/app/shared/component/grid-cards/grid-cards.component.ts
@@ -1,29 +1,64 @@
-import {AfterViewInit, Component, HostListener, Input, OnChanges, SimpleChanges} from '@angular/core';
-
-declare const resizeCards: any;
-declare const masonry: any;
+import {AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-grid-cards',
   templateUrl: './grid-cards.component.html',
   styleUrls: ['./grid-cards.component.scss']
 })
-export class GridCardsComponent implements OnChanges, AfterViewInit {
+export class GridCardsComponent implements AfterViewInit, OnDestroy {
+
+  private subscriptions = new Subscription();
+
+  @ViewChildren('masonryBrick') gridCells: QueryList<ElementRef>;
+  @ViewChild('masonry') grid: ElementRef;
 
   @Input() cardsList: Array<any>;
 
   @HostListener('window:resize')
   reloadMasonry() {
-    masonry('.masonry', '.masonry-brick', 10, 3, 2, 1);
+    this.masonry(10, 3, 2, 1);
   }
 
   ngAfterViewInit(): void {
-    this.reloadMasonry();
+    // on écoute les changements du tableau de cards affichés à l'écran, si il change, on recalcule la hauteur du conteneur
+    this.subscriptions.add(
+      this.gridCells.changes.subscribe(() => {
+        this.reloadMasonry();
+      }));
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['cardsList']) {
-      this.reloadMasonry();
+  /**
+   * Calcul la hauteur nécessaire pour afficher correctement toutes les cards
+   * @param gridGutter
+   * @param dGridCol
+   * @param tGridCol
+   * @param mGridCol
+   */
+  masonry(gridGutter: number, dGridCol: number, tGridCol: number, mGridCol: number) {
+    if (this.gridCells && this.gridCells.length > 0) {
+      const gcLength = this.gridCells.length; // Total number of cells in the masonry
+      let gHeight = 0; // Initial height of our masonry
+      // Calculate the net height of all the cells in the masonry
+      this.gridCells.forEach(elem => {
+        gHeight += elem.nativeElement.offsetHeight + gridGutter;
+      });
+      /*
+       * Calculate and set the masonry height based on the columns
+       * provided for big, medium, and small screen devices.
+       */
+      if (window.screen.width >= 1024) {
+        this.grid.nativeElement.style.height = gHeight / dGridCol + gHeight / (gcLength + 1) + 'px';
+      } else if (window.screen.width < 1024 && window.screen.width >= 768) {
+        this.grid.nativeElement.style.height = gHeight / tGridCol + gHeight / (gcLength + 1) + 'px';
+      } else {
+        this.grid.nativeElement.style.height = gHeight / mGridCol + gHeight / (gcLength + 1) + 'px';
+      }
     }
   }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
 }
